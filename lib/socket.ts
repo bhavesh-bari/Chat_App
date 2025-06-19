baf1555ff932681260a162cd6c7ff332c7fdd5f3
bhave <bhaveshbari0402@gmail.com>
2025-06-19 11:32:37 +0530

    socket deploy

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,10 +1,12 @@
-// lib/socket.ts
 import { io, Socket } from 'socket.io-client';
 
-const socket: Socket = io('http://localhost:4000', {
-  transports: ['websocket'],
-  autoConnect: false, 
-  withCredentials: true,
-});
+const socket: Socket = io(
+  process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:4000',
+  {
+    transports: ['websocket'],
+    autoConnect: false,
+    withCredentials: true,
+  }
+);
 
 export default socket;
